491c7c0eaf9fb9d87edcd7dcb0c7316a427c2a8b
danielamz97 <daniela.montielzuniga@outlook.com>
2024-01-24 00:01:08 -0600

    changes

--- static/js/logic.js
+++ static/js/logic.js
@@ -1,8 +1,8 @@
 
-
+let lineChart = null
 let map = L.map("map", {
     center: [24.0, -102.145],
-    zoom: 3
+    zoom: 5
   })
 
   L.tileLayer('https://{s}.tile-cyclosm.openstreetmap.fr/cyclosm/{z}/{x}/{y}.png', {
@@ -12,9 +12,16 @@ let map = L.map("map", {
 
 
 let dropdownMenu = d3.select("#selDataset") 
+let markersLayer =  L.layerGroup()
+map.addLayer(markersLayer)
 
 
-d3.json('static/js/data.json').then(data => {console.log(data)
+function init(){
+
+optionChanged('Total')
+
+
+d3.json('static/js/data.json').then(data => {
 
 ///////////////////// Populate the Menu /////////////////////////////////
       let groupedData = d3.groups(data, d => d.unidad)
@@ -23,83 +30,119 @@ d3.json('static/js/data.json').then(data => {console.log(data)
         return d[0]
       })
 
-      console.log(uniqueValues)
-
-     
-
       for(let i =0; i<uniqueValues.length; i++){
         dropdownMenu.append("option").text(uniqueValues[i]).property("value",uniqueValues[i])
     }
     
-///////////////////Crear tabla inicial ////////////////////////////////
-  let unidad ='Total'
-  let filteredDataChart = data.filter(function(d) {
-    return d.unidad === unidad
-  })
-  console.log(filteredDataChart)
-  let dataDiv = d3.select('#chart');
-      dataDiv.selectAll('p').remove();
 
-  filteredDataChart.forEach(function(d) {
-    dataDiv.append('p').html('<strong>' + d.tipo_de_gasto + ':</strong> $' + parseFloat(d.monto).toLocaleString('es-MX', {minimumFractionDigits: 2, maximumFractionDigits: 2}));
-  })
-/////////////////// Crear la gráfica inicial //////////////////////////////
-
-  let filteredData = data.filter(function(d) {
-    return d.unidad === unidad && d.tipo_de_gasto!=='Total';
-  })
-
-  filteredData.sort((a, b) => b.monto - a.monto)
-
-  let tipoDeGastoList = []
-  let montoList = []
-
-
-  filteredData.forEach(function(d) {
-    tipoDeGastoList.push(d.tipo_de_gasto)
-    montoList.push(d.monto)
-  })
-
-  let colorScale = d3.scaleSequential(d3.interpolateBlues)
-
-  let bar_trace = [{
-    type: 'bar',
-    x: montoList,
-    y: tipoDeGastoList,
-    orientation: 'h',
-    marker: { 
-      color: montoList.map(value => colorScale(value / Math.max(...montoList))),
-    },
-    text: tipoDeGastoList.map((category, index) => `Tipo de Gasto: ${category}<br>Monto: ${montoList[index].toLocaleString()}`),
-    hoverinfo: 'text',
-  }]
-
-  let bar_layout = {
-    title: 'Total',
-    xaxis: { title: 'Monto' },
-    margin: { t: 50, l: 150, r: 50, b: 50 },
-    bargap: 0.01,
-    bargroupgap: 0.01
-  }
-
-  Plotly.newPlot('plot', bar_trace, bar_layout);
-    
 })
+}
+////////////////////////// Update the map /////////////////////////////////////
+function mapper(unidad){
+  d3.json('static/js/location_data.json').then(data => {console.log(data)
+  
+    markersLayer.clearLayers()
+
+    let filter_data = data.filter(function(d){
+      return d.unidad === unidad})
+    
+    console.log(filter_data)
+
+    
+    let uniques = []
+
+    for(let i =0; i<filter_data.length; i++){
+      let destino = ''
+      destino = filter_data[i].mpio_edo
+      if(uniques.includes(destino)){} else{
+        uniques.push(destino)
+      }
+    }
+
+    console.log(uniques)
+    let filter_destino =[]
+    let num_viajes =[]
+    let coords = []
+    
 
 
-/////////// Función para cambiar info de acuerdo a la selección del usuario de unidad/////////////////////////////////////
+    for(let i =0; i<uniques.length; i++){
+    filter_destino = filter_data.filter(function(d){
+      return d.mpio_edo===uniques[i]})
+
+    console.log(filter_destino[0].mpio_edo)
+    console.log(filter_destino)
+    
+    num_viajes.push(filter_destino.length)
+    coords.push(filter_destino[0].coordenadas)  
+    }
+    
+    
+    for(let i =0; i<coords.length; i++){
+      if(coords[i][1]){
+          markersLayer.addLayer(L.marker([coords[i][1],coords[i][0]],{icon:iconColor(num_viajes[i])}).bindPopup(`
+          <h2>${uniques[i]}</h2><hr>
+          <h3> Total de viajes: ${num_viajes[i].toLocaleString()}</h3>`))
+        }
+    }
+
+  }
+)}
+
+function iconColor(viajes){
+
+  let greenIcon = new L.Icon({
+    iconUrl: 'img/marker-icon-green.png',
+    
+    iconSize: [25, 41],
+    iconAnchor: [12, 41],
+    popupAnchor: [1, -34] 
+  })
+
+  let redIcon = new L.Icon({
+    iconUrl: 'img/marker-icon-red.png',
+    
+    iconSize: [25, 41],
+    iconAnchor: [12, 41],
+    popupAnchor: [1, -34] 
+  })
+
+  let orangeIcon = new L.Icon({
+    iconUrl: 'img/marker-icon-orange.png',
+    
+    iconSize: [25, 41],
+    iconAnchor: [12, 41],
+    popupAnchor: [1, -34] 
+  })
+
+  if(viajes>20){return redIcon} 
+    else if(viajes>10){return orangeIcon}
+    else{return greenIcon}
+
+}
+
+///////////////////////////////////////// Función para cambiar info de acuerdo a la selección del usuario de unidad/////////////////////////////////////
 function optionChanged(selectedValue) {
+  
   d3.json('static/js/data.json').then(function(data){
     let unidad =selectedValue
+   
+    mapper(unidad)
+ 
+    
+
+
 //////////////cambiar cuadro////////////////////////////////////
   let filteredDataChart = data.filter(function(d) {
     return d.unidad === unidad
   })
-  console.log(filteredDataChart)
-  let dataDiv = d3.select('#chart');
-        dataDiv.selectAll('p').remove();
+  
+  let   dataDiv = d3.select('#chart')
+        dataDiv.selectAll('p').remove()
 
-  filteredDataChart.forEach(function(d) {
+        dataDiv.append('p').html('<strong>' +'Unidad de Análisis:' + '</strong> '+ unidad)
+ 
+    filteredDataChart.forEach(function(d) {
     dataDiv.append('p').html('<strong>' + d.tipo_de_gasto + ':</strong> $' + parseFloat(d.monto).toLocaleString('es-MX', {minimumFractionDigits: 2, maximumFractionDigits: 2}));
   })
 ///////////cambiar gráfica///////////
@@ -132,7 +175,7 @@ function optionChanged(selectedValue) {
       hoverinfo: 'text',
     }]
     unidad=selectedValue
-    console.log(unidad)
+    
     let bar_layout = {
       title: `${unidad}`,
       xaxis: { title: 'Monto' },
@@ -143,10 +186,84 @@ function optionChanged(selectedValue) {
 
     Plotly.newPlot('plot', bar_trace, bar_layout)
   })
-  
+//////////// time graph ///////////////
+
+  d3.json('static/js/time_data.json').then(function(data){
+    console.log(data)
+    
+    let unidad = selectedValue
+    console.log(unidad)
+    let filteredData = data.filter(item => item.unidad === unidad);
+
+    let groupedData = d3.group(filteredData, d => d.tipo_de_gasto);
+
+    labels =[]
+    filteredData.forEach(function(d) {
+    labels.push(d.mes)
+    })
+    console.log(labels)
+
+    let datasets = []
+
+    let colorScale = d3.scaleSequential(d3.interpolateRainbow)
+
+    for (const [tipo_de_gasto, group] of groupedData) {
+              let values = Array.from(group, item => item.monto_acumulado);
+              console.log(values)
+              
+              datasets.push({
+                  label: tipo_de_gasto,
+                  data: values,
+                  fill: false,
+                  borderColor: labels.map(value => colorScale(value / Math.max(...labels))),
+                  pointBackgroundColor: 'black',
+                  pointBorderColor: 'black',
+                  pointHitRadius: 10,
+                  hoverBackgroundColor: labels.map(value => colorScale(value / Math.max(...labels))),
+                  pointHoverRadius: 15
+
+              })
+          }
+    
+    if (lineChart) {
+      lineChart.destroy();
+    }
+    let ctx_time = document.getElementById('timeChart').getContext('2d');
+     lineChart = new Chart(ctx_time, {
+        type: 'line',
+        data: {
+            labels: labels,
+            datasets: datasets,
+        },
+        options: {
+          scales: {
+              x: {
+                  type: 'linear',
+                  position: 'bottom',
+              },
+              y: {
+                  beginAtZero: true,
+              },
+          },
+          plugins: {
+            title: {
+              display: true,
+              text: 'Gasto acumulado anual',
+              font: {
+                      size:50,
+                      
+                    }
+            }
+          }
+        },
+    });
+
+
+  })
+
 }
 
-/////gráfica de todos los gastos por Dirección////
+///////////////////////////////////////////////////////gráfica de todos los gastos por Dirección////
 d3.json('static/js/data.json').then(function(jsonData) {
   let filteredData = jsonData.filter(entry => entry.dirección !== "Total" && entry.tipo_de_gasto !== "Total")
 
@@ -186,29 +303,5 @@ d3.json('static/js/data.json').then(function(jsonData) {
   Plotly.newPlot('bigplot', traces, layout);
 })
 
-///////time graph///////
-let margin = {top: 60, right: 230, bottom: 50, left: 50},
-    width = 660 - margin.left - margin.right,
-    height = 400 - margin.top - margin.bottom;
 
-let svg = d3.select('#time_plot')
-    .append("svg")
-    .attr("width", width + margin.left + margin.right)
-    .attr("height", height + margin.top + margin.bottom)
-    .append("g")
-    .attr("transform",
-          "translate(" + margin.left + "," + margin.top + ")");
-
-d3.json('static/js/time_data.json').then(data => {
-  console.log(data)
-  let groupedData = d3.groups(data, d => d.tipo_de_gasto)
-
-  let keys = groupedData.map(function(d) {
-    return d[0]
-  })
-  console.log(keys)
-
-  let color = d3.scaleOrdinal()
-    .domain(keys)
-    .range(d3.schemeSet2);
-})
+init()
